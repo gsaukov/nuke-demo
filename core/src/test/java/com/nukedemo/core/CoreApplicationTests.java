867955b6136fc9cb9b3a476f03ce5aa734f9b38d
Georgy Saukov <georgy.saukov@sap.com>
2023-03-03 21:21:22 +0100

    Raw tests

--- core/src/test/java/com/nukedemo/core/CoreApplicationTests.java
+++ core/src/test/java/com/nukedemo/core/CoreApplicationTests.java
@@ -1,10 +1,7 @@
 package com.nukedemo.core;
 
-import com.nukedemo.core.services.client.OverpassApiClient;
-import hu.supercluster.overpasser.library.output.OutputModificator;
-import hu.supercluster.overpasser.library.output.OutputOrder;
-import hu.supercluster.overpasser.library.output.OutputVerbosity;
-import hu.supercluster.overpasser.library.query.OverpassQuery;
+import com.nukedemo.core.services.nominatim.client.NominatimApiClient;
+import com.nukedemo.core.services.overpass.client.OverpassApiClient;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -12,41 +9,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static hu.supercluster.overpasser.library.output.OutputFormat.JSON;
-
 @SpringBootTest
 @RunWith(SpringRunner.class)
 @Slf4j
 class CoreApplicationTests {
 
+    String overpassQuery1 = "[out:json][timeout:25];(node[\"amenity\"=\"post_box\"](47.48047027491862,19.039797484874725,47.51331674014172,19.07404761761427););out body;>;out skel qt;";
+    String overpassQuery2 = "[out:json][timeout:30]; (node[\"amenity\"=\"parking\"][\"access\"!=\"private\"](47.48047027491862,19.039797484874725,47.51331674014172,19.07404761761427);<;); out body center qt 100;";
+    String overpassQuery3 = "[out:json][timeout:180]; area(id:3601428125)->.searchArea; (way[\"landuse\"=\"military\"](area.searchArea);<;); out body center qt 100;";
+
     @Autowired
-    OverpassApiClient client;
+    OverpassApiClient overpassClient;
+
+    @Autowired
+    NominatimApiClient nominatimClient;
 
     @Test
-    public void testClient() {
-        String query = new OverpassQuery()
-                .format(JSON)
-                .timeout(30)
-                .filterQuery()
-                .node()
-                .amenity("parking")
-                .tagNot("access", "private")
-                .area().rel()
-                .boundingBox(
-                        47.48047027491862, 19.039797484874725,
-                        47.51331674014172, 19.07404761761427
-                )
-                .end()
-                .output(OutputVerbosity.BODY, OutputModificator.CENTER, OutputOrder.QT, 100)
-                .build();
-         log.info(query);
-        String res = client.getOverpassResponse(query);
-        log.info(query);
+    public void testOverpassClient() {
+        log.info(overpassQuery1);
+        String res = overpassClient.interpret(overpassQuery1);
+        log.info(res);
     }
 
+    @Test
+    public void testNominatimClient() {
+        String res = nominatimClient.search("json", "Canada");
+        log.info(res);
+    }
 
-    String validQuery1 = "[out:json][timeout:25];(node[\"amenity\"=\"post_box\"](47.48047027491862,19.039797484874725,47.51331674014172,19.07404761761427););out body;>;out skel qt;";
-    String validQuery2 = "[out:json][timeout:30]; (node[\"amenity\"=\"parking\"][\"access\"!=\"private\"](47.48047027491862,19.039797484874725,47.51331674014172,19.07404761761427);<;); out body center qt 100;";
-    String validQuery3 = "[out:json][timeout:180]; area(id:3601428125)->.searchArea; (way[\"landuse\"=\"military\"](area.searchArea);<;); out body center qt 100;";
 
 }
