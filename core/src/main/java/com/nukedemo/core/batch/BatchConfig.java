1d63515d1488b074c9af7d2bb3ec91ea6264ef63
Georgy Saukov <georgy.saukov@sap.com>
2023-03-03 21:19:55 +0100

    Batch Setup

--- core/src/main/java/com/nukedemo/core/batch/BatchConfig.java
+++ core/src/main/java/com/nukedemo/core/batch/BatchConfig.java
@@ -2,33 +2,39 @@ package com.nukedemo.core.batch;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecutionListener;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.support.DefaultBatchConfiguration;
+import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.repository.support.SimpleJobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.support.transaction.ResourcelessTransactionManager;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionManager;
+
+import javax.sql.DataSource;
 
 
 @Configuration
+//@EnableAutoConfiguration
+@EnableBatchProcessing
 public class BatchConfig {
 
-    @Autowired
-    public JobBuilderFactory jobBuilderFactory;
-
-    @Autowired
-    public StepBuilderFactory stepBuilderFactory;
-
     @Bean
-    public Job processJob() {
-        return jobBuilderFactory.get("processJob")
-                .incrementer(new RunIdIncrementer()).listener(listener())
-                .flow(step()).end().build();
+    public Job processJob(JobRepository jobRepository, Step step) {
+        return new JobBuilder("geo-data-processing-job", jobRepository).incrementer(new RunIdIncrementer()).listener(listener())
+                .flow(step).end().build();
     }
 
     @Bean
-    public Step step() {
-        return stepBuilderFactory.get("orderStep1").<Object, Object> chunk(1)
+    public Step step(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new StepBuilder("data-gathering-step", jobRepository).<Object, Object> chunk(1, transactionManager)
                 .reader(new GeoDataReader())
                 .processor(new GeoDataProcessor())
                 .writer(new GeoDataWriter())
