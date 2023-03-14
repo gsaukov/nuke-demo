accf1d1a83b365251b3caaa2d524e5558b475de3
Georgy Saukov <georgy.saukov@sap.com>
2023-03-14 19:05:39 +0100

    nominatim api client update.

--- core/src/main/java/com/nukedemo/core/services/nominatim/client/NominatimApiClient.java
+++ core/src/main/java/com/nukedemo/core/services/nominatim/client/NominatimApiClient.java
@@ -1,7 +1,6 @@
 package com.nukedemo.core.services.nominatim.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -11,8 +10,12 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
         url = "https://nominatim.openstreetmap.org/")
 public interface NominatimApiClient {
 
-    @RequestMapping(method = GET, value = "search")
-    String search(@RequestParam(name = "format") String format, @RequestParam(name = "q") String q);
+    public static String FORMAT = "geocodejson";
+    public static String FEATURE_TYPE_COUNTRY = "country";
+    public static String FEATURE_TYPE_CITY = "city";
 
-    //https://nominatim.openstreetmap.org/search?format=json&q=Canada
+    @RequestMapping(method = GET, value = "search")
+    String search(@RequestParam(name = "format") String format, @RequestParam(name = "featuretype") String featuretype, @RequestParam(name = "q") String q);
+
+    //https://nominatim.openstreetmap.org/search?format=geocodejson&q=Greenland&featuretype=country
 }
