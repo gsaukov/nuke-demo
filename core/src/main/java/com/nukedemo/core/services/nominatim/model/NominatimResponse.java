57faf25e3aaf0c8d0907df1004d55d48842b602a
Georgy Saukov <georgy.saukov@sap.com>
2023-03-20 19:47:22 +0100

    model erased.

--- core/src/main/java/com/nukedemo/core/services/nominatim/model/NominatimResponse.java
+++
@@ -1,9 +0,0 @@
-package com.nukedemo.core.services.nominatim.model;
-
-import java.util.List;
-import lombok.Data;
-
-@Data
-public class NominatimResponse {
-	private List<NominatimItem> response;
-}
