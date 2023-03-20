c31d9d91cae4da1119d752e85e59874762361a09
Georgy Saukov <georgy.saukov@sap.com>
2023-03-20 19:45:56 +0100

    Batch partitioning and reconfiguration.

--- core/src/main/java/com/nukedemo/core/batch/BatchConfig.java
+++ core/src/main/java/com/nukedemo/core/batch/BatchConfig.java
@@ -4,34 +4,70 @@ import org.springframework.batch.core.JobExecutionListener;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.launch.support.TaskExecutorJobLauncher;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 @EnableBatchProcessing
 public class BatchConfig {
 
+
+    @Autowired
+    JobRepository jobRepository;
+
+    @Autowired
+    PlatformTransactionManager transactionManager;
+
     @Bean
-    public Job processJob(JobRepository jobRepository, Step step) {
+    public JobLauncher  jobLauncher() throws Exception {
+        TaskExecutorJobLauncher jobLauncher = new TaskExecutorJobLauncher();
+        jobLauncher.setJobRepository(jobRepository);
+        jobLauncher.afterPropertiesSet();
+        return jobLauncher;
+    }
+
+    @Bean
+    public Job processJob() {
         return new JobBuilder("geo-data-processing-job", jobRepository)
-                .incrementer(new RunIdIncrementer()).listener(listener())
-                .flow(step)
+                .incrementer(new RunIdIncrementer())
+                .listener(listener())
+                .flow(partitionStep())
                 .end()
                 .build();
     }
 
     @Bean
-    public Step step(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
-        return new StepBuilder("data-gathering-step", jobRepository)
+    public Step partitionStep() {
+        return new StepBuilder("data-processing-partitioning", jobRepository)
+                .partitioner("partition-step", new GeoDataPartitioner())
+                .step(step())
+                .gridSize(4)
+                .taskExecutor(taskExecutor())
+                .build();
+    }
+
+    @Bean
+    public Step step() {
+        return new StepBuilder("data-processing-step", jobRepository)
                 .<String, GeoDataItem> chunk(1, transactionManager)
                 .reader(new GeoDataReader())
                 .processor(new GeoDataProcessor())
                 .writer(new GeoDataWriter())
                 .build();
+    }
+
+    @Bean
+    public TaskExecutor taskExecutor() {
+        return new SimpleAsyncTaskExecutor("spring_batch");
     }
 
     @Bean
