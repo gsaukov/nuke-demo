9e55dec893a0a9628a7c5af9b796677a186b8da7
Georgy Saukov <georgy.saukov@sap.com>
2023-03-26 22:24:26 +0200

    batch reader injection

--- core/src/main/java/com/nukedemo/core/batch/GeoDataReader.java
+++ core/src/main/java/com/nukedemo/core/batch/GeoDataReader.java
@@ -5,6 +5,12 @@ import org.springframework.batch.item.ItemReader;
 
 public class GeoDataReader implements ItemReader<String> {
 
+    private String country;
+
+    public GeoDataReader(String country) {
+        this.country = country;
+    }
+
     @Override
     public String read() {
         return null;
