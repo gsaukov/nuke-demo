2e3879991ce6f07d24a8720ba56ef4a38514a377
Georgy Saukov <georgy.saukov@sap.com>
2023-03-29 19:53:15 +0200

    Turf and Converter

--- core/src/main/java/com/nukedemo/core/services/OsmToGeoJsonConverter.java
+++ core/src/main/java/com/nukedemo/core/services/OsmToGeoJsonConverter.java
@@ -1,62 +1,34 @@
 package com.nukedemo.core.services;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.oracle.truffle.js.scriptengine.GraalJSScriptEngine;
+
+import com.nukedemo.core.services.exceptions.NdException;
+import com.nukedemo.core.services.utils.NdJsonUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.geojson.FeatureCollection;
-import org.graalvm.polyglot.Context;
-import org.graalvm.polyglot.HostAccess;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.script.ScriptEngine;
-import javax.script.ScriptException;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 @Slf4j
 @Service
 public class OsmToGeoJsonConverter {
 
-    //ONLY WORKS WITH GRAALVM OTHERWISE JS ENGINE WILL BE NULL!
-    //    implementation 'org.graalvm.js:js:22.3.1'
-    //    implementation 'org.graalvm.js:js-scriptengine:22.3.1'
+    private static final String OSMTOGEOJSON_LIBRARY = "classpath:scripts/osmtogeojson.js";
 
-    private static final String OSMTOGEOJSON_LS_LIBRARY = "classpath:scripts/osmtogeojson.js";
-
-    private ObjectMapper mapper = new ObjectMapper().registerModule(new JavaTimeModule());
+    @Autowired
+    private GraalVMJsEngineService graalJSScriptEngine;
 
     public FeatureCollection convert(String source) throws Exception {
-        ScriptEngine engine = graalJSScriptEngine();
+        ScriptEngine engine = graalJSScriptEngine.graalJSScriptEngine(OSMTOGEOJSON_LIBRARY);
         engine.put("data", source);
         String res = engine.eval("JSON.stringify(osmtogeojson(JSON.parse(data)))").toString();
         log.info(res);
         return convertOSMtoGeoJSON(res);
     }
 
-    private ScriptEngine graalJSScriptEngine() throws IOException, ScriptException {
-        ScriptEngine engine = GraalJSScriptEngine.create(
-                null,
-                Context.newBuilder("js")
-                        .allowHostAccess(HostAccess.NONE)
-                        .allowAllAccess(false)
-                        .allowHostClassLookup(s -> false));
-        Path path = new PathMatchingResourcePatternResolver().getResource(OSMTOGEOJSON_LS_LIBRARY).getFile().toPath();
-        engine.eval(Files.newBufferedReader(path, StandardCharsets.UTF_8));
-        return engine;
-    }
-
-    private FeatureCollection convertOSMtoGeoJSON(String source) {
-        FeatureCollection sample = null;
-        try {
-            sample = mapper.readValue(source, FeatureCollection.class);
-        } catch (IOException e) {
-            log.error("Deserialization failed", e);
-        }
-        return sample;
+    private FeatureCollection convertOSMtoGeoJSON(String source) throws NdException {
+        return NdJsonUtils.fromJson(source, FeatureCollection.class);
     }
 
 }
---
+++ core/src/main/java/com/nukedemo/core/services/TurfGeospatialService.java
@@ -0,0 +1,32 @@
+package com.nukedemo.core.services;
+
+
+import com.nukedemo.core.services.exceptions.NdException;
+import com.nukedemo.core.services.utils.NdJsonUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.geojson.FeatureCollection;
+import org.geojson.Polygon;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.script.ScriptEngine;
+import java.math.BigDecimal;
+
+@Slf4j
+@Service
+public class TurfGeospatialService {
+
+    private static final String TURF_LIBRARY = "classpath:scripts/turf.js";
+
+    @Autowired
+    private GraalVMJsEngineService graalJSScriptEngine;
+
+    public BigDecimal calculateArea(Polygon source) throws Exception {
+        ScriptEngine engine = graalJSScriptEngine.graalJSScriptEngine(TURF_LIBRARY);
+        engine.put("data", source);
+        String res = engine.eval("JSON.stringify(turf.area(turf.polygon(data)))").toString();
+        log.info(res);
+        return new BigDecimal(res);
+    }
+
+}
