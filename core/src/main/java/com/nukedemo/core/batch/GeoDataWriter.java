ff04c78cb91f8f264560d07c5e65d43c856db904
Georgy Saukov <georgy.saukov@sap.com>
2023-09-21 18:53:53 +0200

    Writer adjustments.

--- core/src/main/java/com/nukedemo/core/batch/GeoDataWriter.java
+++ core/src/main/java/com/nukedemo/core/batch/GeoDataWriter.java
@@ -7,6 +7,7 @@ import org.springframework.batch.item.ItemWriter;
 import org.springframework.stereotype.Service;
 
 import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
@@ -17,25 +18,34 @@ import java.util.UUID;
 @StepScope
 public class GeoDataWriter implements ItemWriter<GeoDataItem> {
 
+    private static final String OSM_FOLDER = "./res/osm/";
+    private static final String GEOJSON_FOLDER = "./res/geojson/";
+    private static final String OSM_EXTENSION = "_osm.json";
+    private static final String GEOJSON_EXTENSION = ".geojson";
+
     private UUID uuid = UUID.randomUUID();
+
+    public GeoDataWriter() throws IOException {
+        Files.createDirectories(Paths.get(OSM_FOLDER));
+        Files.createDirectories(Paths.get(GEOJSON_FOLDER));
+    }
 
     @Override
     public void write(Chunk<? extends GeoDataItem> chunk) throws Exception {
         //write chunks of objects to documents
-        Files.createDirectories(Paths.get("./res"));
         for(GeoDataItem item : chunk.getItems()){
 //            log.info("Writer ID:" + uuid + " Item: " + item.getCountryGeoJson());
-            writeToFile(item.getCountryName(), item.getCountryOsm(), "_osm.json");
-            writeToFile(item.getCountryName(), item.getCountryGeoJson(), ".geojson");
+            writeToFile(OSM_FOLDER + item.getCountryName() + OSM_EXTENSION, item.getCountryOsm());
+            writeToFile(GEOJSON_FOLDER + item.getCountryName() + GEOJSON_EXTENSION, item.getCountryGeoJson());
         }
     }
 
-    private void writeToFile(String countryName, String data, String extension) {
-        try (FileWriter writer = new FileWriter("./res/" + countryName + extension);) {
+    private void writeToFile(String fileName, String data) {
+        try (FileWriter writer = new FileWriter(fileName);) {
             writer.write(data);
         }
         catch (Exception e) {
-            log.error("Failed to create file for: " + countryName, e);
+            log.error("Failed to create file for: " + fileName, e);
         }
     }
 
