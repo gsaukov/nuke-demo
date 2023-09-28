121049de4d7250b609d091f8e586cdf179003327
Georgy Saukov <georgy.saukov@sap.com>
2023-09-28 18:48:45 +0200

    GeoCalculatorApp pretty printing.

--- geo-calculator/src/main/java/com/nukedemo/geocalculator/services/GeoCalculatorApp.java
+++ geo-calculator/src/main/java/com/nukedemo/geocalculator/services/GeoCalculatorApp.java
@@ -1,12 +1,14 @@
 package com.nukedemo.geocalculator.services;
 
-import com.mapbox.geojson.Feature;
-import com.mapbox.geojson.FeatureCollection;
-import com.mapbox.geojson.Geometry;
-import com.mapbox.geojson.Point;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.mapbox.geojson.*;
+import com.mapbox.geojson.gson.GeoJsonAdapterFactory;
 import com.mapbox.turf.TurfMeasurement;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Slf4j
 @Service
@@ -14,9 +16,9 @@ public class GeoCalculatorApp {
 
     private static final String PROPERTY_PREFIX = "ND:";
 
-    public String calculate (String geojson) {
-        FeatureCollection featureCollection = FeatureCollection.fromJson(geojson);
-        for(Feature feature : featureCollection.features()) {
+    public String calculate(String geojson) throws IOException {
+        FeatureCollection featureCollection = readJson(geojson);
+        for (Feature feature : featureCollection.features()) {
             preCalculate(feature);
         }
         return featureCollection.toJson();
@@ -24,7 +26,7 @@ public class GeoCalculatorApp {
 
     private void preCalculate(Feature feature) {
         Geometry g = feature.geometry();
-        Point center = (Point)(TurfMeasurement.center(feature).geometry());
+        Point center = (Point) (TurfMeasurement.center(feature).geometry());
         addProperty(feature, "center", center.toJson());
         addProperty(feature, "area", String.valueOf(TurfMeasurement.area(feature)));
     }
@@ -32,6 +34,15 @@ public class GeoCalculatorApp {
     private static void addProperty(Feature feature, String name, String value) {
         feature.addStringProperty(PROPERTY_PREFIX + name, value);
 
+    }
+
+    private FeatureCollection readJson(String json) {
+        Gson gson = new GsonBuilder()
+                .registerTypeAdapterFactory(GeoJsonAdapterFactory.create())
+                .registerTypeAdapterFactory(GeometryAdapterFactory.create())
+                .setPrettyPrinting()
+                .create();
+        return gson.fromJson(json, FeatureCollection.class);
     }
 
 
