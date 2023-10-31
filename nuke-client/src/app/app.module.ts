800549a16c0f1b91f985fea4024353953c2ce773
Georgy Saukov <georgy.saukov@sap.com>
2023-10-31 18:09:04 +0100

    Nuke-client draft page template

--- nuke-client/src/app/app.module.ts
+++ nuke-client/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { MapPageComponent } from './main-page/map-page/map-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    MapPageComponent
   ],
   imports: [
     BrowserModule
