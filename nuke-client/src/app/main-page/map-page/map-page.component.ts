0dff60fa20c6d3fac40eb35be7b296022be23aff
Georgy Saukov <georgy.saukov@sap.com>
2023-10-31 19:18:20 +0100

    Nuke-client Openlayer map draft

--- nuke-client/src/app/main-page/map-page/map-page.component.ts
+++ nuke-client/src/app/main-page/map-page/map-page.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
 
 @Component({
   selector: 'app-map-page',
   templateUrl: './map-page.component.html',
   styleUrls: ['./map-page.component.css']
 })
-export class MapPageComponent {
+export class MapPageComponent implements OnInit {
 
-}
+  private map!: Map;
+
+  ngOnInit(): void {
+    this.map = new Map({
+      view: new View({
+        center: [0, 0],
+        zoom: 1,
+      }),
+      layers: [
+        new TileLayer({
+          source: new OSM(
+            {
+              attributions: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }),
+        }),
+      ],
+      target: 'ol-map'
+    });
+  }}
