f1fa2d0c51a5083b1846be10209e93d523f1abf6
Georgy Saukov <georgy.saukov@sap.com>
2023-11-04 15:37:34 +0100

    Nuke-client removed schem restrictions.

--- nuke-client/src/app/app.module.ts
+++ nuke-client/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -15,6 +15,10 @@ import { MapPageComponent } from './main-page/map-page/map-page.component';
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA,
+    NO_ERRORS_SCHEMA
+  ]
 })
 export class AppModule { }
