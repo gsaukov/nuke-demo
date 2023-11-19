b50efa8ea5e37a91b8fd7b4efa36fdf959ce8104
Georgy Saukov <georgy.saukov@sap.com>
2023-11-19 21:57:17 +0100

    Nuke-client calculator form

--- nuke-client/src/app/main-page/map-page/calculator/calculator.component.ts
+++ nuke-client/src/app/main-page/map-page/calculator/calculator.component.ts
@@ -12,13 +12,18 @@ export class CalculatorComponent {
   @Input() map!: Map;
   form: FormGroup
 
-
   constructor() {
     this.form = new FormGroup({
-      start: new FormControl(null, [Validators.required]),
-      end: new FormControl(null, [Validators.required]),
+      cityName: new FormControl(null, [Validators.required]),
+      radius: new FormControl(200, [Validators.required]),
+      number: new FormControl(5, [Validators.required]),
     })
   }
 
-  onSubmit() {}
+  onSubmit() {
+    const cityName = this.form.controls['cityName'].value
+    const radius = this.form.controls['radius'].value
+    const number = this.form.controls['number'].value
+    console.log(cityName + ' ' + radius + ' ' + number)
+  }
 }
