4cbcf6bfc733bb1ab5de04159a98f75f6553eca7
Georgy Saukov <georgy.saukov@sap.com>
2023-11-24 19:18:30 +0100

    Nuke-client calculator

--- nuke-client/src/app/main-page/map-page/calculator/calculator.component.ts
+++ nuke-client/src/app/main-page/map-page/calculator/calculator.component.ts
@@ -1,6 +1,12 @@
 import {Component, Input} from '@angular/core';
 import Map from 'ol/Map';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {NominatimService} from "../../../services/nominatim.service";
+import {OverpassService} from "../../../services/overpass.service";
+import {MapService} from "../../../services/map.service";
+import {Feature as TurfFeature, MultiPolygon, Polygon} from "@turf/turf";
+import {Observable} from "rxjs";
+import * as osm2geojson from 'osm2geojson-lite';
 
 @Component({
   selector: 'app-calculator',
@@ -9,21 +15,51 @@ import {FormControl, FormGroup, Validators} from "@angular/forms";
 })
 export class CalculatorComponent {
 
+  private COUNTRY_ID_PREFIX = "36";
+
   @Input() map!: Map;
   form: FormGroup
 
-  constructor() {
+  constructor(private mapService: MapService, private nominatimService: NominatimService, private overpassService: OverpassService) {
     this.form = new FormGroup({
       cityName: new FormControl(null, [Validators.required]),
-      radius: new FormControl(200, [Validators.required]),
-      number: new FormControl(5, [Validators.required]),
+      radius: new FormControl(null, [Validators.required]),
+      number: new FormControl(null, [Validators.required]),
     })
   }
 
   onSubmit() {
+    this.form.disable()
     const cityName = this.form.controls['cityName'].value
     const radius = this.form.controls['radius'].value
     const number = this.form.controls['number'].value
     console.log(cityName + ' ' + radius + ' ' + number)
+
+    this.nominatimService.getCityData('Hamburg')
+      .subscribe(nominatimRes => this.getOverpassData(nominatimRes)
+        .subscribe(overpassRes => this.printOnMap(overpassRes))
+      );
+
+    this.form.enable()
   }
+
+  private getOverpassData (data:any):Observable<any> {
+    console.log(data)
+    const countryId = this.getOverpassCountryId(data[0].osm_id)
+    return this.overpassService.getGeometryData(countryId);
+  }
+
+  private getOverpassCountryId(osmId:number) {
+    const formatted = String(osmId).padStart(8, '0');
+    return this.COUNTRY_ID_PREFIX + formatted;
+  }
+
+  private printOnMap (overpassRes:any) {
+    console.log(JSON.stringify(overpassRes))
+    let geoJsonObject = osm2geojson(overpassRes, {completeFeature:true});
+    console.log(JSON.stringify(geoJsonObject))
+    this.mapService.addGeometryLayer(this.map, geoJsonObject)
+    this.mapService.addCircles(this.map, geoJsonObject.features[0] as TurfFeature<(Polygon | MultiPolygon)>, 3000, 200)
+  }
+
 }
--- nuke-client/src/app/main-page/map-page/map-page.component.ts
+++ nuke-client/src/app/main-page/map-page/map-page.component.ts
@@ -1,12 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import Map from 'ol/Map';
 import {MapService} from "../../services/map.service";
-import {Feature as TurfFeature, MultiPolygon, Polygon} from "@turf/turf";
-import {NominatimService} from "../../services/nominatim.service";
-import {OverpassService} from "../../services/overpass.service";
-import {Observable} from "rxjs";
-import * as osm2geojson from 'osm2geojson-lite';
-
 
 @Component({
   selector: 'app-map-page',
@@ -15,42 +9,13 @@ import * as osm2geojson from 'osm2geojson-lite';
 })
 export class MapPageComponent implements OnInit {
 
-  private COUNTRY_ID_PREFIX = "36";
-
   private _map!: Map;
 
-  constructor(private mapService: MapService, private nominatimService: NominatimService, private overpassService: OverpassService) {
+  constructor(private mapService: MapService) {
   }
 
   ngOnInit(): void {
     this._map = this.mapService.buildMap(this._map);
-    this.nominatimService.getCityData('Hamburg')
-      .subscribe(nominatimRes => this.getOverpassData(nominatimRes)
-        .subscribe(overpassRes => this.printOnMap(overpassRes))
-      );
-
-    // this.mapService.addGeometryLayer(this.map, this.mapService.geoJsonObject)
-    // this.mapService.addCircles(this.map, this.mapService.geoJsonObject.features[0], 10, 2000)
-  }
-
-
-  private getOverpassData (data:any):Observable<any> {
-    console.log(data)
-    const countryId = this.getOverpassCountryId(data[0].osm_id)
-    return this.overpassService.getGeometryData(countryId);
-  }
-
-  private getOverpassCountryId(osmId:number) {
-    const formatted = String(osmId).padStart(8, '0');
-    return this.COUNTRY_ID_PREFIX + formatted;
-  }
-
-  private printOnMap (overpassRes:any) {
-    console.log(JSON.stringify(overpassRes))
-    let geoJsonObject = osm2geojson(overpassRes, {completeFeature:true});
-    console.log(JSON.stringify(geoJsonObject))
-    this.mapService.addGeometryLayer(this._map, geoJsonObject)
-    this.mapService.addCircles(this._map, geoJsonObject.features[0] as TurfFeature<(Polygon | MultiPolygon)>, 3000, 200)
   }
 
   get map(): Map {
