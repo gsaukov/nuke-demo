dcc6baba9ba96bf4293cbe0d9ce5767ff0cb2723
Georgy Saukov <georgy.saukov@sap.com>
2024-01-27 20:59:43 +0100

    nuke demo population gh2 and

---
+++ popuplation-batch/src/main/java/com/nukedemo/population/h2/H2ServerConfig.java
@@ -0,0 +1,20 @@
+package com.nukedemo.population.h2;
+
+import org.h2.tools.Server;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.sql.SQLException;
+
+@Configuration
+@ConditionalOnProperty(value="populationBatch.h2Enabled", havingValue = "true")
+public class H2ServerConfig {
+
+//    conn string jdbc:h2:tcp://localhost:9090/file:./data/db/nke-demo-db
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public Server inMemoryH2DatabaseServer() throws SQLException {
+        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9090");
+    }
+
+}
