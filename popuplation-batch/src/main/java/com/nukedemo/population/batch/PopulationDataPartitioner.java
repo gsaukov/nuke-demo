cf26d53806ebac8ebc277124560cc49093014768
Georgy Saukov <georgy.saukov@sap.com>
2024-01-30 20:33:24 +0100

    nuke demo population batch partitioner implementation.

--- popuplation-batch/src/main/java/com/nukedemo/population/batch/PopulationDataPartitioner.java
+++ popuplation-batch/src/main/java/com/nukedemo/population/batch/PopulationDataPartitioner.java
@@ -1,7 +1,9 @@
 package com.nukedemo.population.batch;
 
 import com.nukedemo.population.services.clients.ghsl.GhslApiClient;
+import feign.Response;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.ListUtils;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.partition.support.Partitioner;
 import org.springframework.batch.item.ExecutionContext;
@@ -30,14 +32,38 @@ public class PopulationDataPartitioner implements Partitioner {
 
     @Override
     public Map<String, ExecutionContext> partition(int gridSize) {
+        List<PopulationInputItem> items = getItems();
+        return executions(gridSize, items);
+    }
+
+    private List<PopulationInputItem> getItems() {
+        List<PopulationInputItem> items = new ArrayList<>();
+        for (int row = 1; row <= maxRow; row++) {
+            for (int column = 1; column <= maxColumn; column++) {
+                if (checkFileExists(row, column)) {
+                    items.add(new PopulationInputItem(row, column));
+                }
+            }
+        }
+        return items;
+    }
+
+    private Map<String, ExecutionContext> executions(int gridSize, List<PopulationInputItem> items) {
+        int itemsPerPartition = items.size() / gridSize + 1;
+        List<List<PopulationInputItem>> partitions = ListUtils.partition(items, itemsPerPartition);
         Map<String, ExecutionContext> result = new HashMap<>();
-        ExecutionContext context = new ExecutionContext();
-        List<PopulationInputItem> partition = new ArrayList<>();
-        PopulationInputItem item = new PopulationInputItem(maxRow, maxColumn);
-        partition.add(item);
-        context.put("area", new ArrayList<>(partition));
-        result.put("area", context);
+        for (int i = 0; i < partitions.size(); i++) {
+            ExecutionContext context = new ExecutionContext();
+            List<PopulationInputItem> partition = partitions.get(i);
+            context.put("area", new ArrayList<>(partition));
+            result.put("partition_" + i, context);
+        }
         return result;
+    }
+
+    private boolean checkFileExists(int row, int column) {
+        Response fileExists = ghslApiClient.checkFileExists(row, column);
+        return fileExists.status() == 200;
     }
 
 }
