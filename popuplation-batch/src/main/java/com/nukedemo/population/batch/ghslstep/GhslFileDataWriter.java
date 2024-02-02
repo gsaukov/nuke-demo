13805be696a0a918b6e9869386244ce356005a71
Georgy Saukov <georgy.saukov@sap.com>
2024-02-02 19:58:57 +0100

    nuke demo population batch ghsl writer improved

--- popuplation-batch/src/main/java/com/nukedemo/population/batch/ghslstep/GhslFileDataWriter.java
+++ popuplation-batch/src/main/java/com/nukedemo/population/batch/ghslstep/GhslFileDataWriter.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
