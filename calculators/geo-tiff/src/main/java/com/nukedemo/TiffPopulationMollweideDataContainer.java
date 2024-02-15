4a53aea3ff0bf38540da1840da29da735f184665
Georgy Saukov <georgy.saukov@sap.com>
2024-02-15 18:28:19 +0100

    nuke demo Tiff mollwide calculations and container

--- calculators/geo-tiff/src/main/java/com/nukedemo/TiffPopulationMollweideDataContainer.java
+++ calculators/geo-tiff/src/main/java/com/nukedemo/TiffPopulationMollweideDataContainer.java
@@ -1,21 +1,20 @@
 package com.nukedemo;
 
-
 import org.geotools.coverage.grid.GridCoordinates2D;
 import org.geotools.coverage.grid.GridCoverage2D;
 import org.geotools.coverage.grid.GridGeometry2D;
+import org.geotools.data.PrjFileReader;
 import org.geotools.gce.geotiff.GeoTiffReader;
 import org.geotools.geometry.DirectPosition2D;
-import org.geotools.referencing.crs.DefaultGeographicCRS;
 import org.opengis.geometry.DirectPosition;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
-
 import java.awt.image.Raster;
 import java.io.File;
+import java.io.FileInputStream;
+import java.nio.channels.FileChannel;
 
 public class TiffPopulationMollweideDataContainer {
-    public static CoordinateReferenceSystem wgs84 = DefaultGeographicCRS.WGS84;
-
+    public CoordinateReferenceSystem mollweide;
     private final GeoTiffReader reader;
     private final GridCoverage2D cov;
     private final Raster tiffRaster;
@@ -23,16 +22,18 @@ public class TiffPopulationMollweideDataContainer {
     private final int  rasterHeight;
 
     public TiffPopulationMollweideDataContainer(String fileToProcess) throws Exception {
+        this.mollweide = getCrs();
         this.reader = new GeoTiffReader(new File(fileToProcess));
         this.cov = reader.read(null);
-        this.tiffRaster = cov.getRenderedImage().getData();
+        //use tif raster minx/miny/ to find
+        this.tiffRaster = cov.getRenderedImage().getTile(0, 0);
         this.rasterWidth = tiffRaster.getWidth();
         this.rasterHeight = tiffRaster.getHeight();
     }
 
     public double[] pixelDataFromCoord(double lat, double lon) throws Exception {
         GridGeometry2D gg = cov.getGridGeometry();
-        DirectPosition2D posWorld = new DirectPosition2D(wgs84, lat, lon);
+        DirectPosition2D posWorld = new DirectPosition2D(mollweide, lat, lon);
         GridCoordinates2D posGrid = gg.worldToGrid(posWorld);
         double[] rasterData = new double[1];
         tiffRaster.getPixel(posGrid.x, posGrid.y, rasterData);
@@ -47,13 +48,22 @@ public class TiffPopulationMollweideDataContainer {
 
     public GridCoordinates2D coordFromXY(double lat, double lon) throws Exception {
         GridGeometry2D gg = cov.getGridGeometry();
-        DirectPosition2D posWorld = new DirectPosition2D(wgs84, lat, lon); // longitude supplied first
+        DirectPosition2D posWorld = new DirectPosition2D(mollweide, lat, lon); // longitude supplied first
         return gg.worldToGrid(posWorld);
     }
 
     public DirectPosition xyFromCoord(int x, int y) throws Exception {
         GridCoordinates2D coord = new GridCoordinates2D(x, y);
         return cov.getGridGeometry().gridToWorld(coord);
+    }
+
+    static CoordinateReferenceSystem getCrs() throws Exception {
+        FileInputStream instream= null;
+        instream = new FileInputStream(new File("./src/main/resources/GHS_POP_E2030_GLOBE_R2023A_54009_100_V1_0_R7_C18.prj"));
+        final FileChannel channel = instream.getChannel();
+        PrjFileReader projReader  = new PrjFileReader(channel);
+        CoordinateReferenceSystem crs = projReader.getCoordinateReferenceSystem();
+        return crs;
     }
 
 }
