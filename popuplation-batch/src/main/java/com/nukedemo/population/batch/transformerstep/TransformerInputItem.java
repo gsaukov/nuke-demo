41ae8b6656a0fe33179a84d25ebd64b1ebb5a847
Georgy Saukov <georgy.saukov@sap.com>
2024-03-04 20:12:23 +0100

    nuke demo ghsl pop Transformation step Input item.

---
+++ popuplation-batch/src/main/java/com/nukedemo/population/batch/transformerstep/TransformerInputItem.java
@@ -0,0 +1,10 @@
+package com.nukedemo.population.batch.transformerstep;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class TransformerInputItem implements Serializable {
+
+}
