1ffc5b84f85be897d6680137574e2f6429ed0061
Georgy Saukov <georgy.saukov@sap.com>
2024-03-13 20:53:16 +0100

    nuke demo geo calculator JtsCalculationServiceTest union

---
+++ calculators/geo-calculator/src/test/java/com/nukedemo/geocalculator/services/JtsCalculationServiceTest.java
@@ -0,0 +1,39 @@
+package com.nukedemo.geocalculator.services;
+
+import com.mapbox.geojson.FeatureCollection;
+import com.mapbox.geojson.Geometry;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+class JtsCalculationServiceTest {
+
+    private static String GEOJSON;
+
+    private static FeatureCollection FEATURE_COLLECTION;
+
+    JtsCalculationService service;
+
+    @BeforeAll
+    static void beforeAll() throws IOException, ParseException {
+        Path path = new PathMatchingResourcePatternResolver().getResource("jts_union_hole.json").getFile().toPath();
+        GEOJSON = Files.readString(path, StandardCharsets.UTF_8);
+        FEATURE_COLLECTION = FeatureCollection.fromJson(GEOJSON);
+    }
+
+    @BeforeEach
+    void setUp() {
+        service = new JtsCalculationService();
+    }
+
+    @Test
+    void union() throws Exception {
+        Geometry geometry = service.union(FEATURE_COLLECTION.features());
+    }
+}
