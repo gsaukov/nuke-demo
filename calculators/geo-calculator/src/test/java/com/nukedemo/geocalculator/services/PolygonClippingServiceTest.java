82d70f2a2e6f30f453d6cce031cbbb84680351fd
Georgy Saukov <georgy.saukov@sap.com>
2024-03-14 22:18:13 +0100

    nuke demo geo calculator PolygonClippingServiceTest draft

---
+++ calculators/geo-calculator/src/test/java/com/nukedemo/geocalculator/services/PolygonClippingServiceTest.java
@@ -0,0 +1,17 @@
+package com.nukedemo.geocalculator.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PolygonClippingServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void union() {
+    }
+}
