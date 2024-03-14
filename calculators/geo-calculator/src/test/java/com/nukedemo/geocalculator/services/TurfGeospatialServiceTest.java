3245c6ba767496a4a1b21570cf311444dd181361
Georgy Saukov <georgy.saukov@sap.com>
2024-03-14 15:03:07 +0100

    nuke demo geo calculator TurfGeospatialServiceTest union

---
+++ calculators/geo-calculator/src/test/java/com/nukedemo/geocalculator/services/TurfGeospatialServiceTest.java
@@ -0,0 +1,44 @@
+package com.nukedemo.geocalculator.services;
+
+import com.mapbox.geojson.FeatureCollection;
+import com.mapbox.geojson.Geometry;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.script.ScriptException;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.*;
+@Slf4j
+class TurfGeospatialServiceTest {
+
+    private static String GEOJSON;
+
+    private static FeatureCollection FEATURE_COLLECTION;
+
+    TurfGeospatialService service;
+
+    @BeforeAll
+    static void beforeAll() throws IOException {
+        Path path = new PathMatchingResourcePatternResolver().getResource("jts_union_hole.json").getFile().toPath();
+        GEOJSON = Files.readString(path, StandardCharsets.UTF_8);
+        FEATURE_COLLECTION = FeatureCollection.fromJson(GEOJSON);
+    }
+
+    @BeforeEach
+    void setUp() throws ScriptException, IOException {
+        service = new TurfGeospatialService(new GraalVMJSScriptingEngineService());
+    }
+
+    @Test
+    void union() throws Exception {
+        Geometry geometry = service.union(FEATURE_COLLECTION.features());
+        log.info(geometry.toJson());
+    }
+}
