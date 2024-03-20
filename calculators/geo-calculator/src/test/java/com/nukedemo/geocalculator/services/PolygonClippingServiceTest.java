29b79159bcdf9c29dabc5f5f59e3e19dc00df6fe
Georgy Saukov <georgy.saukov@sap.com>
2024-03-20 19:02:59 +0100

    nuke demo geo calculator PolygonClippingService unionMerge test adjustment.

--- calculators/geo-calculator/src/test/java/com/nukedemo/geocalculator/services/PolygonClippingServiceTest.java
+++ calculators/geo-calculator/src/test/java/com/nukedemo/geocalculator/services/PolygonClippingServiceTest.java
@@ -3,7 +3,6 @@ package com.nukedemo.geocalculator.services;
 import com.mapbox.geojson.FeatureCollection;
 import com.mapbox.geojson.Geometry;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
@@ -18,18 +17,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @Slf4j
 class PolygonClippingServiceTest {
 
-    private static String GEOJSON;
-
-    private static FeatureCollection FEATURE_COLLECTION;
-
     PolygonClippingService service;
-
-    @BeforeAll
-    static void beforeAll() throws IOException {
-        Path path = new PathMatchingResourcePatternResolver().getResource("jts_union_hole.json").getFile().toPath();
-        GEOJSON = Files.readString(path, StandardCharsets.UTF_8);
-        FEATURE_COLLECTION = FeatureCollection.fromJson(GEOJSON);
-    }
 
     @BeforeEach
     void setUp() {
@@ -37,8 +25,37 @@ class PolygonClippingServiceTest {
     }
 
     @Test
-    void union() throws Exception {
-        Geometry geometry = service.union(FEATURE_COLLECTION.features());
-        log.info(geometry.toJson());
+    void unionMergeSimple() throws Exception {
+        FeatureCollection featureCollection = getFeatureCollection("union_hole_simple.json");
+        Geometry geometry = service.unionMerge(featureCollection.features());
+        log.info("union merge simple: " + geometry.toJson());
+    }
+
+    @Test
+    void unionMergeAdvanced() throws Exception {
+        FeatureCollection featureCollection = getFeatureCollection("union_hole_double.json");
+        Geometry geometry = service.unionMerge(featureCollection.features());
+        log.info("union merge advance: " + geometry.toJson());
+    }
+
+    @Test
+    void unionSimple() throws Exception {
+        FeatureCollection featureCollection = getFeatureCollection("union_hole_simple.json");
+        Geometry geometry = service.union(featureCollection.features());
+        log.info("union simple: " + geometry.toJson());
+    }
+
+    @Test
+    void unionAdvanced() throws Exception {
+        FeatureCollection featureCollection = getFeatureCollection("union_hole_double.json");
+        Geometry geometry = service.union(featureCollection.features());
+        log.info("union advanced: " + geometry.toJson());
+    }
+
+
+    FeatureCollection getFeatureCollection(String fileName) throws IOException {
+        Path path = new PathMatchingResourcePatternResolver().getResource(fileName).getFile().toPath();
+        String geoJson = Files.readString(path, StandardCharsets.UTF_8);
+        return FeatureCollection.fromJson(geoJson);
     }
 }
