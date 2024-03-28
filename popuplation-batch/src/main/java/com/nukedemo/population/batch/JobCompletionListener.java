cc587f3d00de27912c10d799db800b81fcf5913f
Georgy Saukov <georgy.saukov@sap.com>
2024-03-28 22:07:13 +0100

    nuke demo pop batch JobCompletionListener metadata population.

--- popuplation-batch/src/main/java/com/nukedemo/population/batch/JobCompletionListener.java
+++ popuplation-batch/src/main/java/com/nukedemo/population/batch/JobCompletionListener.java
@@ -1,22 +1,37 @@
 package com.nukedemo.population.batch;
 
+import com.nukedemo.GhslMetaData;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.listener.JobExecutionListenerSupport;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+import java.io.File;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
+import static com.nukedemo.shared.utils.NdJsonUtils.toJson;
+
 @Slf4j
+@Component
 public class JobCompletionListener extends JobExecutionListenerSupport {
+
+    private final String resultFolder;
 
     private final AtomicLong pixelWithValueCount;
 
     private final AtomicLong pixelTotalValue;
 
-    public JobCompletionListener() {
-        pixelWithValueCount = new AtomicLong();
-        pixelTotalValue = new AtomicLong();
+    private final ConcurrentHashMap<String, GhslMetaData> metaData;
+
+    public JobCompletionListener(@Value("${populationBatch.ghsl.resultFolder}") String resultFolder) {
+        this.resultFolder = resultFolder;
+        this.pixelWithValueCount = new AtomicLong();
+        this.pixelTotalValue = new AtomicLong();
+        this.metaData = new ConcurrentHashMap<>();
     }
 
     @Override
@@ -25,6 +40,7 @@ public class JobCompletionListener extends JobExecutionListenerSupport {
             log.info("BATCH JOB COMPLETED SUCCESSFULLY");
             log.info("Total pixels with data count: " + pixelWithValueCount.get());
             log.info("Total data value: " + pixelTotalValue.get());
+            writeMetaData();
         }
     }
 
@@ -34,6 +50,18 @@ public class JobCompletionListener extends JobExecutionListenerSupport {
 
     public void addValue (int value) {
         pixelTotalValue.addAndGet(value);
+    }
+
+    public void addMetaData(String label, GhslMetaData ghslMetaData) {
+        metaData.put(label, ghslMetaData);
+    }
+
+    private void writeMetaData() {
+        try {
+            FileUtils.writeStringToFile(new File(resultFolder, "metaData.json"), toJson(metaData));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
