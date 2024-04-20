3abc921cb03a6f36e08042b6b89b5765713b759e
Georgy Saukov <georgy.saukov@sap.com>
2024-04-20 21:27:15 +0200

    nuke demo tif calculator TiffPopulationDataContainer 3ss Test

--- popuplation-batch/src/main/java/com/nukedemo/population/batch/layercompressionstep/LayerCompressionDataPartitioner.java
+++ popuplation-batch/src/main/java/com/nukedemo/population/batch/layercompressionstep/LayerCompressionDataPartitioner.java
@@ -1,6 +1,10 @@
 package com.nukedemo.population.batch.layercompressionstep;
 
+import com.fasterxml.jackson.databind.type.MapType;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+import com.nukedemo.GhslMetaData;
 import com.nukedemo.population.batch.shared.BatchUtils;
+import com.nukedemo.shared.utils.NdJsonUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.partition.support.Partitioner;
@@ -10,9 +14,7 @@ import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static com.nukedemo.population.batch.layercompressionstep.LayerCompressionStepConfiguration.SOURCE_PATH;
 
@@ -31,6 +33,11 @@ public class LayerCompressionDataPartitioner implements Partitioner {
 
     @Value("${populationBatch.ghsl.maxColumn}")
     private int maxColumn;
+
+    private List<Double> topLeftLons;
+    private List<Double> topLeftLats;
+    private List<Double> bottomRightLons;
+    private List<Double> bottomRightLats;
 
     public LayerCompressionDataPartitioner() {
     }
@@ -52,17 +59,55 @@ public class LayerCompressionDataPartitioner implements Partitioner {
     }
 
     private List<LayerCompressionInputItem> getInputItems() throws IOException {
+        Map<String, GhslMetaData> meta = getMetaData();
+        calculateGrid(meta);
         List<LayerCompressionInputItem> items = new ArrayList<>();
         for (int row = 1; row < maxRow; row = row + BLOCK_SIZE) {
             for (int column = 1; column < maxColumn; column = column + BLOCK_SIZE) {
                 List<List<String>> block = getBlock(row, column);
                 if(block != null){
-                    LayerCompressionInputItem item = new LayerCompressionInputItem(block);
+                    double [][] blockDimensions = calculateBlockDimensions(row, column);
+                    LayerCompressionInputItem item = new LayerCompressionInputItem(block, blockDimensions);
                     items.add(item);
                 }
             }
         }
         return items;
+    }
+
+    private double[][] calculateBlockDimensions(int row, int column) {
+        int lonIndex = column - 1;
+        int latIndex = row - 1;
+        int step = BLOCK_SIZE - 1;
+        return new double[][] {
+                {topLeftLons.get(lonIndex), topLeftLats.get(latIndex)},
+                {bottomRightLons.get(lonIndex + step), bottomRightLats.get(latIndex + step)}
+        };
+    }
+
+    private Map<String, GhslMetaData> getMetaData() throws IOException {
+        File metaDataFile = new File(SOURCE_PATH + "/" + ORIGINAL_RESOLUTION + "/", "metaData.json");
+        MapType ref = TypeFactory.defaultInstance().constructMapType(HashMap.class, String.class, GhslMetaData.class);
+        return NdJsonUtils.MAPPER.readValue(metaDataFile, ref);
+    }
+
+    private void calculateGrid(Map<String, GhslMetaData> meta) {
+        TreeSet<Double> sortedTopLeftLons = new TreeSet<>();
+        TreeSet<Double> sortedTopLeftLats = new TreeSet<>();
+        TreeSet<Double> sortedBottomRightLons = new TreeSet<>();
+        TreeSet<Double> sortedBottomRightLats = new TreeSet<>();
+        for (GhslMetaData val :meta.values()) {
+            sortedTopLeftLons.add(val.getTopLeftCorner()[0]);
+            sortedTopLeftLats.add(val.getTopLeftCorner()[1]);
+            sortedBottomRightLons.add(val.getBottomRightCorner()[0]);
+            sortedBottomRightLats.add(val.getBottomRightCorner()[1]);
+        }
+        topLeftLons = new ArrayList<>(sortedTopLeftLons);
+        topLeftLats = new ArrayList<>(sortedTopLeftLats);
+        Collections.sort(topLeftLats, Collections.reverseOrder());
+        bottomRightLons = new ArrayList<>(sortedBottomRightLons);
+        bottomRightLats = new ArrayList<>(sortedBottomRightLats);
+        Collections.sort(bottomRightLats, Collections.reverseOrder());
     }
 
     private List<List<String>> getBlock(int rowStart, int columnStart) {
@@ -92,12 +137,5 @@ public class LayerCompressionDataPartitioner implements Partitioner {
     private String getGhslKey(int row, int column) {
         return "GHS_POP_E2025_GLOBE_R2023A_" + ORIGINAL_RESOLUTION + "_V1_0_R" + row + "_C" + column;
     }
-
-
-    // [
-    //  [DUMMY,                                              DUMMY,                                              DUMMY],
-    //  [GHS_POP_E2025_GLOBE_R2023A_4326_30ss_V1_0_R2_C1,    GHS_POP_E2025_GLOBE_R2023A_4326_30ss_V1_0_R2_C2,    GHS_POP_E2025_GLOBE_R2023A_4326_30ss_V1_0_R2_C3],
-    //  [GHS_POP_E2025_GLOBE_R2023A_4326_30ss_V1_0_R3_C1,    GHS_POP_E2025_GLOBE_R2023A_4326_30ss_V1_0_R3_C2,    GHS_POP_E2025_GLOBE_R2023A_4326_30ss_V1_0_R3_C3]
-    // ]
 
 }
