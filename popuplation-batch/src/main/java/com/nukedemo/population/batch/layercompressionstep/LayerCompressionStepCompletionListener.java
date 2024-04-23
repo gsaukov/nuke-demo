acf8043cb1c56afb202c41b35fd86300e71b871d
Georgy Saukov <georgy.saukov@sap.com>
2024-04-23 20:28:06 +0200

    nuke demo pop layer compression step completion listener fix

--- popuplation-batch/src/main/java/com/nukedemo/population/batch/layercompressionstep/LayerCompressionStepCompletionListener.java
+++ popuplation-batch/src/main/java/com/nukedemo/population/batch/layercompressionstep/LayerCompressionStepCompletionListener.java
@@ -4,24 +4,21 @@ package com.nukedemo.population.batch.layercompressionstep;
 import com.nukedemo.GhslMetaData;
 import org.apache.commons.io.FileUtils;
 import org.springframework.batch.core.*;
-import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicLong;
 
 import static com.nukedemo.population.batch.layercompressionstep.LayerCompressionStepConfiguration.TARGET_RESOLUTION;
 import static com.nukedemo.population.batch.populationstep.PopulationDataWriter.POPULATION_IMG_FOLDER;
 import static com.nukedemo.shared.utils.NdJsonUtils.toJson;
 
 @Component
-@StepScope
 public class LayerCompressionStepCompletionListener implements StepExecutionListener {
 
     @Value("${populationBatch.ghsl.basePath}")
-    private int basePath;
+    private String basePath;
 
     private final ConcurrentHashMap<String, GhslMetaData> metaData;
 
