12f2df84ba775b07b999d84c6ff6eb225d5f22da
Georgy Saukov <georgy.saukov@sap.com>
2024-05-06 20:22:39 +0200

    nuke demo tif calculator split image test

--- calculators/geo-tiff/src/test/java/com/nukedemo/TiffPopulationDataContainer3ssTest.java
+++ calculators/geo-tiff/src/test/java/com/nukedemo/TiffPopulationDataContainer3ssTest.java
@@ -7,6 +7,8 @@ import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.nukedemo.TiffPngConverter.DENSITY_3SS;
 import static org.junit.jupiter.api.Assertions.*;
@@ -41,6 +43,28 @@ class TiffPopulationDataContainer3ssTest {
             fos.write(res);
         }
         outputFile.delete();
+    }
+
+    @Test
+    public void testImageSplitting() throws Exception {
+        BufferedImage imageToSplit = ImageIO.read(new File("./src/main/resources/GHS_POP_E2025_GLOBE_R2023A_4326_3ss_V1_0_R4_C19.png"));
+        List<List<BufferedImage>> splitImages = ImageTransformations.splitImage(imageToSplit, 3);
+        List<File> tempFiles = new ArrayList<>();
+        for(int i = 0; i<splitImages.size(); i++) {
+            List<BufferedImage> row = splitImages.get(i);
+            for(int j = 0; j<row.size(); j++) {
+                BufferedImage image = row.get(j);
+                ByteArrayOutputStream out = new ByteArrayOutputStream();
+                ImageIO.write(image, "PNG", out);
+                File outputFile = new File("test_res_"+j+"_"+i+".png");
+                byte[] res = out.toByteArray();
+                try (FileOutputStream fos = new FileOutputStream(outputFile)) {
+                    fos.write(res);
+                }
+                tempFiles.add(outputFile);
+            }
+        }
+        tempFiles.forEach(f->f.delete());
     }
 
 }
